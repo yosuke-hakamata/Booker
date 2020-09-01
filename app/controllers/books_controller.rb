4bbdaaf517f2c77839d45e15daaf4d118a6c9041
yosuke-hakamata <67750575+yosuke-hakamata@users.noreply.github.com>
2020-09-01 20:59:17 +0900

    [Finish]4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  		redirect_to book_path(@book), notice:'Book was successfully added'
+  	else
+  		@books=Book.all
+  		render "index"
+  	end
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    	redirect_to book_path(@book), notice:'Book was successfully edited'
+    else
+    	render "edit"
+    end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+    if book.destroy
+    	redirect_to books_path, notice:'Book was successfully destroyed'
+    else
+    	render "index"
+    end
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
